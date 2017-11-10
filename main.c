37280f9905b79be8f55e85be3a3d95a54712c45c
JeongHwan <jhlee51047@naver.com>
2017-11-10 05:38:36 -0800

    Add Menual

--- main.c
+++ main.c
@@ -5,5 +5,19 @@ void main()
   int a;
   printf("Choose Game!!\n");
   printf("1. BBopgi Game      2. Rock Scissors Paper\n");
-  scanf("%d",&a);
+  
+  while(1){
+    scanf("%d",&a);
+    if( a == 1){
+      //bbopgi();
+      break;
+    }
+    else if( a == 2){
+      //RSP();
+      break;
+    }
+    else{
+      printf("Please Choose 1~2\n");
+    }
+  }
 }
